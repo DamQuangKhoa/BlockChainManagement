3c7a425e7b4570dabf530fe3237507f6f2a2851d
TED <ted@TEDs-MacBook-Pro.local>
2019-08-11 19:29:22 +0700

    update

--- src/layouts/review/Home.jsx
+++ src/layouts/review/Home.jsx
@@ -4,7 +4,8 @@ import {
   ContractData,
   ContractForm
 } from "drizzle-react-components";
-
+import * as TableOption from "./ConstantsCommon";
+import Table from "../../component/Table/Table";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -25,29 +26,35 @@ class Home extends Component {
         <div className="pure-g">
           <div className="pure-u-1-1">
             <h2>Active Account</h2>
-            <AccountData accountIndex="0" units="ether" precision="3" />
+            {/* <AccountData accountIndex="0" units="ether" precision="3" /> */}
 
             <br />
             <br />
           </div>
+
           <div className="pure-u-1-1">
             <h2>Review</h2>
-            <p>Danh Sach thông tin lập trình viên đã lưu vào Blockchain</p>
+            <p>Danh Sach thông tin đánh giá </p>
+            <Table
+              headerTable={TableOption.PERSONNEL_TABLE_STRING}
+              title={TableOption.PERSONNEL_TITLE_STRING}
+              prototype={TableOption.PERSONNEL_TABLE_PROTOTYPE}
+            />
+            {/* <ContractForm contract="ReviewFactory" method="_testString" /> */}
             <p>
-              <ContractData contract="ReviewFactory" method="test" />
+              {/* <ContractData contract="ReviewFactory" method="test" /> */}
               {/* <ContractData contract="ReviewFactory" method="test2" /> */}
               <strong>List Reviews</strong>:{" "}
               {data.map((v, i) => {
                 return (
-                  <ContractData
+                  {/* <ContractData
                     contract="ReviewFactory"
                     method="reviewsSecond"
                     methodArgs={[i]}
-                  />
+                  /> */}
                 );
               })}
             </p>
-            <ContractForm contract="ReviewFactory" method="_testString" />
 
             <br />
             <br />
